816b76d466a44e4cfa226d26743b74644f857d4f
Brett Chalupa <brettchalupa@gmail.com>
2024-12-17 09:18:28 -0500

    Add chrome exntesion

---
+++ chrome_ext/README.md
@@ -0,0 +1,3 @@
+# dlm Chrome Extension
+
+Add to Chrome as an unpacked extension and click the icon to send a given tab to dlm.
---
+++ chrome_ext/background.js
@@ -0,0 +1,33 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.action.setBadgeText({
+        text: "",
+    });
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+    const currentUrl = tab.url;
+    const url = 'http://brettch-everest.nord:8001/api/add-urls'; // TODO: make this configurable
+
+    await chrome.action.setBadgeText({
+        tabId: tab.id,
+        text: "...",
+    });
+    await fetch(url, {
+        method: 'POST',
+        mode: "no-cors",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ urls: [currentUrl] })
+    })
+        .then(response => {
+            console.log('Request successful', response);
+        })
+        .catch(error => {
+            console.error('Request failed', error);
+        });
+    await chrome.action.setBadgeText({
+        tabId: tab.id,
+        text: "",
+    });
+});
